b7a06d282da77e2f05a3385544cc4d37926875d2
BrussoBaum <sb@brussobaum.de>
2019-11-15 14:11:15 +0100

    1

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,8 +1,9 @@
 <?php snippet('header') ?>
 
-<!-- <h1><?= $page->title()->html() ?></h1> -->
+
 
 <?php snippet('cursor') ?>
+<section class="content"><h1><?= $page->title()->html() ?></h1></section>
 
 <?php snippet('navigation') ?>
 
