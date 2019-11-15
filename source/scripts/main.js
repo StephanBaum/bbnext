fb46867632af4bf6bdc9aaeb0ecca4345b859662
BrussoBaum <sb@brussobaum.de>
2019-11-15 09:48:28 +0100

    gsap

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,10 +1,34 @@
 'use strict';
 
+import './partials/cursor';
+import './partials/magnetize';
+
+/*
+ * GSAP ..
+ */
+var window = {}
+  , navigator = { userAgent: "" }
+  , dummyElement = { style: {}, getElementsByTagName: function() { return [] } }
+  , document = { createElement: function() { return dummyElement } };
+
+// START OF FILE
+
+// END OF FILE
+
+module.exports = _gsScope;
+
+/*
+ * END GSAP ..
+ */
+
+
+
 /*
  * Importing functions ..
  */
 
 import featureDetection from './partials/featureDetection';
+
 
 
 /*
