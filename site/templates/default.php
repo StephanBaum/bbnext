7048f0344b6f057ae9761395551899bedcd5ac24
BrussoBaum <sb@brussobaum.de>
2019-11-15 09:54:02 +0100

    1

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,5 +1,10 @@
 <?php snippet('header') ?>
 
-<h1><?= $page->title()->html() ?></h1>
+<!-- <h1><?= $page->title()->html() ?></h1> -->
+
+<?php snippet('cursor') ?>
+
+<?php snippet('navigation') ?>
+
 
 <?php snippet('footer') ?>
